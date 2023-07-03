5b501483440a7d2132a65f49e33c5a26683a83c6
Abdul Hakim Ghaniy <abdulhakimghaniy@outlook.co.id>
2023-07-03 12:05:08 +0700

    Fix link_tree argument must be a directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 append :linked_files, 'config/mongoid.yml', 'config/master.key'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor', 'storage'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/javascript', 'storage'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
